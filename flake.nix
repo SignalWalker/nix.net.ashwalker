dc6b2eb391e6f70e5e2037e2505b2bd408f79d4e
Ash Walker <ashurstwalker@gmail.com>
2022-11-11 12:13:23 -0500

    feat: ashwalker.net

--- flake.nix
+++ flake.nix
@@ -6,6 +6,35 @@
       url = github:kamadorueda/alejandra;
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    sysbase = {
+      url = github:signalwalker/nix.sys.base;
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.alejandra.follows = "alejandra";
+      inputs.homebase.follows = "homebase";
+      inputs.homelib.follows = "homelib";
+    };
+    homelib = {
+      url = github:signalwalker/nix.home.lib;
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.alejandra.follows = "alejandra";
+      inputs.home-manager.follows = "home-manager";
+    };
+    homebase = {
+      url = github:signalwalker/nix.home.base;
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.alejandra.follows = "alejandra";
+      inputs.homelib.follows = "homelib";
+      inputs.home-manager.follows = "home-manager";
+    };
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # pkgs
+    ashwalker-net = {
+      url = "gitlab:signalwalker/ashwalker-net";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = inputs @ {
     self,
@@ -16,5 +45,22 @@
       std = nixpkgs.lib;
     in {
       formatter = std.mapAttrs (system: pkgs: pkgs.default) inputs.alejandra.packages;
+      signalModules.default = {
+        name = "sys.net.ashwalker";
+        dependencies = signal.flake.set.toDependencies {
+          flakes = inputs;
+          filter = [];
+          outputs = {
+            sysbase.nixosModules = ["default"];
+            ashwalker-net.nixosModules = ["default"];
+          };
+        };
+        outputs = dependencies: {
+          nixosModules = {lib, ...}: {
+            imports = [./nixos-module.nix];
+            config = {};
+          };
+        };
+      };
     };
 }
