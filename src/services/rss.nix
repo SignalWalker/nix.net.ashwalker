62b77ab10dc97427a995653a25d6077c58377cfc
Ash Walker <ashurstwalker@gmail.com>
2022-11-12 10:18:32 -0500

    secrets

--- src/services/rss.nix
+++ src/services/rss.nix
@@ -6,6 +6,7 @@
 }:
 with builtins; let
   std = pkgs.lib;
+  vhost = "rss.${config.networking.fqdn}";
 in {
   options = with lib; {};
   disabledModules = [];
@@ -16,28 +17,38 @@ in {
       pubSubHubbub.enable = true;
       selfUrlPath = "https://${config.services.tt-rss.virtualHost}";
       registration = {
-        enable = true;
+        enable = false;
         notifyAddress = "ash@ashwalker.net";
         maxUsers = 0;
       };
-      auth = {
-        autoLogin = false;
-      };
-      virtualHost = "rss.${config.networking.fqdn}";
+      virtualHost = vhost;
     };
-    # services.freshrss = {
-    # 	enable = true;
-    # 	virtualHost = "rss.${config.networking.fqdn}";
-    # 	baseUrl = "https://${config.services.freshrss.virtualHost}";
-    # 	database = {
-    # 		type = "sqlite";
-    # 		passFile = config.age.secrets.freshrssDbPassword.path;
-    # 	};
-    # 	defaultUser = "ash";
-    # 	passwordFile = config.age.secrets.freshrssPassword.path;
-    # };
-    # age.secrets.freshrssPassword.owner = "freshrss";
-    # age.secrets.freshrssDbPassword.owner = "freshrss";
+	age.secrets.ttrssEnvironment.file = ../../secrets/ttrssEnvironment;
+	systemd.services.phpfpm-tt-rss = {
+		serviceConfig = {
+			EnvironmentFile = age.secrets.ttrssEnvironment.path;
+		};
+	};
+	services.rss-bridge = {
+		enable = true;
+		user = "rssbridge";
+		group = "rssbridge";
+		virtualHost = "bridge.${vhost}";
+		whitelist = [ "*" ];
+	};
+	# services.freshrss = {
+	# 	enable = true;
+	# 	virtualHost = "rss.${config.networking.fqdn}";
+	# 	baseUrl = "https://${config.services.freshrss.virtualHost}";
+	# 	database = {
+	# 		type = "sqlite";
+	# 		passFile = config.age.secrets.freshrssDbPassword.path;
+	# 	};
+	# 	defaultUser = "ash";
+	# 	passwordFile = config.age.secrets.rssUserPassword.path;
+	# };
+	# age.secrets.freshrssPassword.owner = "freshrss";
+	# age.secrets.freshrssDbPassword.owner = "freshrss";
     services.nginx.virtualHosts."${config.services.tt-rss.virtualHost}" = {
       enableACME = true;
       forceSSL = true;
