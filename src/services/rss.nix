b3a5f972bb41174c15c4beb98c33e3b5ad34b663
Ash Walker <ashurstwalker@gmail.com>
2022-11-12 10:18:41 -0500

    style: nix fmt

--- src/services/rss.nix
+++ src/services/rss.nix
@@ -23,32 +23,32 @@ in {
       };
       virtualHost = vhost;
     };
-	age.secrets.ttrssEnvironment.file = ../../secrets/ttrssEnvironment;
-	systemd.services.phpfpm-tt-rss = {
-		serviceConfig = {
-			EnvironmentFile = age.secrets.ttrssEnvironment.path;
-		};
-	};
-	services.rss-bridge = {
-		enable = true;
-		user = "rssbridge";
-		group = "rssbridge";
-		virtualHost = "bridge.${vhost}";
-		whitelist = [ "*" ];
-	};
-	# services.freshrss = {
-	# 	enable = true;
-	# 	virtualHost = "rss.${config.networking.fqdn}";
-	# 	baseUrl = "https://${config.services.freshrss.virtualHost}";
-	# 	database = {
-	# 		type = "sqlite";
-	# 		passFile = config.age.secrets.freshrssDbPassword.path;
-	# 	};
-	# 	defaultUser = "ash";
-	# 	passwordFile = config.age.secrets.rssUserPassword.path;
-	# };
-	# age.secrets.freshrssPassword.owner = "freshrss";
-	# age.secrets.freshrssDbPassword.owner = "freshrss";
+    age.secrets.ttrssEnvironment.file = ../../secrets/ttrssEnvironment;
+    systemd.services.phpfpm-tt-rss = {
+      serviceConfig = {
+        EnvironmentFile = age.secrets.ttrssEnvironment.path;
+      };
+    };
+    services.rss-bridge = {
+      enable = true;
+      user = "rssbridge";
+      group = "rssbridge";
+      virtualHost = "bridge.${vhost}";
+      whitelist = ["*"];
+    };
+    # services.freshrss = {
+    # 	enable = true;
+    # 	virtualHost = "rss.${config.networking.fqdn}";
+    # 	baseUrl = "https://${config.services.freshrss.virtualHost}";
+    # 	database = {
+    # 		type = "sqlite";
+    # 		passFile = config.age.secrets.freshrssDbPassword.path;
+    # 	};
+    # 	defaultUser = "ash";
+    # 	passwordFile = config.age.secrets.rssUserPassword.path;
+    # };
+    # age.secrets.freshrssPassword.owner = "freshrss";
+    # age.secrets.freshrssDbPassword.owner = "freshrss";
     services.nginx.virtualHosts."${config.services.tt-rss.virtualHost}" = {
       enableACME = true;
       forceSSL = true;
