7bdf0bd703db4a0e732e92ac91958bc22129403a
Ash Walker <ashurstwalker@gmail.com>
2022-11-14 13:48:36 -0500

    fix(sourcehut): ???

--- src/services/src.nix
+++ src/services/src.nix
@@ -11,53 +11,52 @@ in {
   options = with lib; {};
   disabledModules = [];
   imports = [];
-  config =
-    std.recursiveUpdate {
-      services.sourcehut = {
-        enable = false;
+  config = {
+    services.sourcehut = {
+      enable = true;
 
-        postgresql.enable = config.services.postgresql.enable;
-        postfix.enable = config.services.postfix.enable;
+      postgresql.enable = config.services.postgresql.enable;
+      postfix.enable = config.services.postfix.enable;
 
-        settings."sr.ht" = {
-          environment = "production";
-          global-domain = domain;
-          origin = "https://${domain}";
-          network-key = config.age.secrets.srcNetworkKey.path;
+      settings."sr.ht" = {
+        environment = "production";
+        global-domain = domain;
+        origin = "https://${domain}";
+        network-key = config.age.secrets.srcNetworkKey.path;
+      };
+      settings.webhooks.private-key = config.age.secrets.srcWebhookKey.path;
+
+      nginx = {
+        enable = config.services.nginx.enable;
+        virtualHost = {
+          enableACME = true;
+          forceSSL = true;
         };
-        settings.webhooks.private-key = config.age.secrets.srcWebhookKey.path;
-
-        nginx = {
-          enable = config.services.nginx.enable;
-          virtualHost = {
-            enableACME = true;
-            forceSSL = true;
-          };
-        };
-
-        meta.enable = true;
-        settings."meta.sr.ht" = {};
-
-        git.enable = true;
-        settings."git.sr.ht" = {};
-
-        hg.enable = false;
-        settings."hg.sr.ht" = {};
-
-        hub.enable = false;
-        settings."hub.sr.ht" = {};
-
-        todo.enable = false;
-        settings."todo.sr.ht" = {};
       };
 
-      services.nginx.virtualHosts."${domain}".enableACME = true;
-    } (foldl' (acc: nxt: std.recursiveUpdate acc nxt) {} (map (srvc: let
-        srvdn = "${srvc}.${domain}";
-      in {
-        security.acme.certs.${domain}.extraDomainNames = [srvdn];
-        services.nginx.virtualHosts."${srvdn}".useACMEHost = domain;
-      })
-      config.sourcehut.services));
+      meta.enable = true;
+      settings."meta.sr.ht" = {};
+
+      git.enable = true;
+      settings."git.sr.ht" = {};
+
+      hg.enable = true;
+      settings."hg.sr.ht" = {};
+
+      hub.enable = false;
+      settings."hub.sr.ht" = {};
+
+      todo.enable = false;
+      settings."todo.sr.ht" = {};
+    };
+
+    security.acme.certs.${domain}.extraDomainNames = map (srv: "${srv}.${domain}") config.sourcehut.services;
+
+    services.nginx.virtualHosts =
+      foldl'
+      (acc: srvc: std.recursiveUpdate acc {"meta.${domain}".useACMEHost = domain;})
+      {"${domain}".enableACME = true;}
+      config.sourcehut.services;
+  };
   meta = {};
 }
