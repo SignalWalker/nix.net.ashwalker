bc5a7de287a94fc66208a3aa3bb3c33e30ae736b
Ash Walker <ashurstwalker@gmail.com>
2022-12-08 13:29:17 -0500

    begin nextcloud config

---
+++ src/services/cloud.nix
@@ -0,0 +1,51 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with builtins; let
+  std = pkgs.lib;
+  hostName = "cloud.${config.networking.fqdn}";
+  cfg = config.signal.services.cloud;
+  nc = config.services.nextcloud;
+in {
+  options.signal.services.cloud = with lib; {
+    enable = mkEnableOption "cloud";
+  };
+  disabledModules = [];
+  imports = [];
+  config = lib.mkIf cfg.enable {
+    services.nextcloud = {
+      enable = true;
+      inherit hostName;
+      https = true;
+      config = {
+        dbtype = "pgsql";
+        dbuser = "nextcloud";
+        dbhost = "/run/postgresql";
+        dbname = "nextcloud";
+        adminpassFile = config.age.secrets.cloudAdminPassword.path;
+        adminuser = "admin";
+      };
+    };
+    services.nginx.virtualHosts.${hostName} = {
+      enableACME = true;
+      forceSSL = true;
+    };
+    services.postgresql = {
+      ensureDatabases = [nc.config.dbname];
+      ensureUsers = [
+        {
+          name = nv.config.dbuser;
+          ensurePermissions."DATABASE ${nc.config.dbname}" = "ALL PRIVILEGES";
+        }
+      ];
+    };
+    systemd.services."nextcloud-setup" = {
+      requires = ["postgresql.service"];
+      after = ["postgresql.service"];
+    };
+  };
+  meta = {};
+}
