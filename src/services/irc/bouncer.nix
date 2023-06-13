2b3e14b77b2c9e27b31a6d896f05e4a55b84ccda
Ash Walker <ashurstwalker@gmail.com>
2023-06-13 16:40:47 -0400

    feat(irc.bouncer)

--- src/services/irc/bouncer.nix
+++ src/services/irc/bouncer.nix
@@ -107,22 +107,29 @@ in {
         useLegacyConfig = false;
         openFirewall = false;
         modulePackages = [
-          (pkgs.writeText "loadpassfile.py" ''
-            import znc
+          (pkgs.stdenvNoCC.mkDerivation {
+            pname = "loadpassfile.py";
+            src = pkgs.writeText "loadpassfile.py" ''
+              import znc
 
-            class loadpassfile(znc.Module):
-              description = "Load user password from a file"
-              module_types = [znc.CModInfo.UserModule]
-              has_args = True
+              class loadpassfile(znc.Module):
+                description = "Load user password from a file"
+                module_types = [znc.CModInfo.UserModule]
+                has_args = True
 
-              def OnLoad(self, args, message):
-                with open(args) as passfile:
-                  hash = passfile.readline()
-                  salt = passfile.readline()
-                  znc.GetUser().SetPass(hash, znc.CUser.HASH_SHA256, salt)
-                  return True
-                return False
-          '')
+                def OnLoad(self, args, message):
+                  with open(args) as passfile:
+                    hash = passfile.readline()
+                    salt = passfile.readline()
+                    znc.GetUser().SetPass(hash, znc.CUser.HASH_SHA256, salt)
+                    return True
+                  return False
+            '';
+            dontUnpack = true;
+            installPhase = ''
+              cp $src $out
+            '';
+          })
         ];
         config = {
           LoadModule = ["modpython"];
