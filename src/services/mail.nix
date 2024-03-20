b25fda33f77eaa541b9e6c2498f777a636b9375c
Ash Walker <ash@ashwalker.net>
2024-03-20 17:06:58 -0400

    feat(activitypub): disable activitypub

--- src/services/mail.nix
+++ src/services/mail.nix
@@ -18,6 +18,8 @@ in {
     age.secrets.mailPasswordDaemon = {
       file = ./mail/mailPasswordDaemon.age;
     };
+    # workaround for https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/issues/275
+    services.dovecot2.sieve.extensions = ["fileinto"];
     # from `simple-nixos-mailserver`
     mailserver = {
       enable = true;
@@ -25,6 +27,7 @@ in {
       sendingFqdn = config.networking.fqdn;
       certificateScheme = "acme-nginx";
       domains = [config.networking.fqdn];
+      localDnsResolver = false;
       loginAccounts = {
         "ash@${config.networking.fqdn}" = {
           hashedPasswordFile = config.age.secrets.mailPasswordAsh.path;
