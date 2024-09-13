6d0e1e94947b3a2fd9b43fc9a69689042b1e8916
Ash Walker <ashurstwalker@gmail.com>
2024-09-13 19:23:13 -0400

    ...

--- src/services/rss/secrets.nix
+++ src/services/rss/secrets.nix
@@ -1,5 +1,5 @@
 let
-  srv-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJkq7D+tBr4IrT0eZhggfWOLxsM9l28vLoYt3ZTMsgHd host@ashwalker.net";
+  srv-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL96LFIgKgNXAQPl9y/EtWwxBZtRatxGk535ZxDy/IU5 root@exampleHost";
   ash = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJFqg4NlJu7u1pcCel3EZshVwUxIfwpsh2fxhaQlLAar";
   keys = [srv-host ash];
 in {
